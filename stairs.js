4ca0fb8a5832b26beb00b3d93ba1d0c334e2879a
Vladimir Gurovic <gurovic@gmail.com>
2014-04-14 09:29:25 +0400

    multiple inputs per stair, unreachable stairs

--- stairs.js
+++ stairs.js
@@ -2,7 +2,7 @@
 
 function showAnswer(event) {
     event.userAnswer.each(function(i, element) {
-        $(element).val(event.correctAnswer[event.userAnswer.length - 1 - i]).removeClass('ok').removeClass('fail');
+        $(element).val(event.correctAnswer[$(element).attr('data-i')]).removeClass('ok').removeClass('fail');
         event.problemDiv.find('button.check').hide();
     });
 }
@@ -11,9 +11,10 @@ function showAnswer(event) {
 function check(event) {
     event.problemDiv.removeClass('fail').addClass('ok');
     event.userAnswer.each(function(i, element) {
-        if($(element).val() !== "" && $(element).val() == event.correctAnswer[event.userAnswer.length - 1 - i]) {
+        if($(element).val() !== "" && $(element).val() == event.correctAnswer[$(element).attr('data-i')]) {
             $(element).addClass('ok').removeClass('fail');
         } else {
+            event.correctAnswer[$(element)["data-i"]]
             $(element).addClass('fail').removeClass('ok');
             event.problemDiv.removeClass('ok').addClass('fail');
         }
@@ -23,23 +24,51 @@ function check(event) {
 
 
 function coin_element(value) {
-    if(value !== null) {
+    if(value && value !== '∞') {
         return "<div class='circle'>" + value + "</div>";
     } else {
         return "";
     }
 }
 
+
+function default_value(value) {
+    if(value === '∞') {
+        return "'∞' readonly='readonly'";
+    } else {
+        return "''";
+    }
+}
+
+
+function stair_class(value) {
+    if(value === '∞') {
+        return 'removed';
+    } else {
+        return '';
+    }
+}
+
+function input(inputPerStair, tabId, i, answer) {
+    result = '';
+    console.log(answer, i);
+    for(var j = 0; j < inputPerStair; j++) {
+        var result = result + "<input tabindex='" + (tabId + inputPerStair * i + j + 1) + "' class='answer' value =" +
+            default_value(answer[inputPerStair * i + j]) + " data-i='" + (inputPerStair * i + j) + "'>";
+    }
+    console.log(result, i);
+    return result
+}
 function Stairs(options) {
     var tabId = Math.round(Math.random()* 100 + 100) * 100
     var self = this.table = $('<table class="stairs" width="' + (options.height * 61 + 62) + '"></table>');
     this.table.append("<tr><td colspan=" + (options.height) + "></td><td class='upstair'>" + coin_element(options.price[options.height]) + "</div>" + (options.height) + "</td></tr>")
     for(var i = options.height; i >= 2; i--) {
         this.table.append("<tr><td colspan=" + (i - 1) + "></td><td class='upstair'>" + coin_element(options.price[i - 1]) +
-            (i - 1) + "</td><td class='stair'><input tabindex='" + (tabId + i + 1) + "' class='answer'></td></tr>")
+            (i - 1) + "</td><td class='stair " + stair_class(options.answer[i]) + "'>" + input(options.inputPerStair, tabId, i, options.answer) + "</td></tr>")
     }
-    this.table.append("<tr><td class='upstair'><img src='img/boy.gif'>0</td><td class='stair'><input tabindex='" + (tabId + 2) +"' class='answer'></td></tr>");
-    this.table.append("<tr><td class='floor-stair'><input tabindex='" + (tabId + 1) + "' class='answer'></td><td align='right' colspan=" + (options.height)+
+    this.table.append("<tr><td class='upstair'><img src='img/boy.gif'>0</td><td class='stair'>" + input(options.inputPerStair, tabId, 1, options.answer) + "</td></tr>");
+    this.table.append("<tr><td class='floor-stair'>" + input(options.inputPerStair, tabId, 0, options.answer) + "</td><td align='right' colspan=" + (options.height)+
         "><button tabindex='" + (tabId + 98) + "' class='check'>Проверить</button><button tabindex='"+ (tabId + 99) + "' class='answer'>Правильный ответ</button></td></tr>");
     this.div = $("#" + options.id);
     this.div.append(this.table);
@@ -75,7 +104,6 @@ function Stairs(options) {
 }
 
 function init_dp() {
-    $('body').on("showAnswer", showAnswer);
-    $('body').on("check", check);
+    $('body').on("showAnswer", showAnswer).on("check", check);
 }
 
